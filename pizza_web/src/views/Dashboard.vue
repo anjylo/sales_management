015b6912595be702c65cec92d857ed3c3f5dee7c
angelo <gelo.guimba@gmail.com>
2025-07-13 01:56:55 +0800

    include logout button to dashboard

--- pizza_web/src/views/Dashboard.vue
+++ pizza_web/src/views/Dashboard.vue
@@ -1,23 +1,32 @@
 <script setup>
-  import { onMounted, ref } from 'vue';
-  import { fetchUser } from '@utils/auth'
+import { onMounted, ref } from 'vue'
+import { fetchUser, logout } from '@utils/auth'
+import { useRouter } from 'vue-router'
 
-  const name = ref('')
-  const email = ref('')
+const name = ref('')
+const email = ref('')
+const router = useRouter()
 
-  const handleFetchUser = async () => {
-    const user = await fetchUser()
+const handleFetchUser = async () => {
+  const user = await fetchUser()
 
-     if (user) {
-      name.value = user.name
-      email.value = user.email
-    }
+  if (user) {
+    name.value = user.name
+    email.value = user.email
   }
+}
 
-  onMounted(async () => {
-    handleFetchUser()
-  })
+const handleLogout = async () => {
+  if (await logout()) {
+    router.push({ name: 'Login' })
+  }
+}
+
+onMounted(() => {
+  handleFetchUser()
+})
 </script>
+
 <template>
   <v-container class="fill-height d-flex align-center justify-center">
     <v-navigation-drawer permanent>
@@ -25,9 +34,12 @@
         prepend-icon="mdi-account-circle"
         :title="name"
         :subtitle="email"
+        class="my-3"
       />
+
       <v-divider />
-      <v-list density="compact" nav>
+
+      <v-list density="comfortable" nav>
         <v-list-item 
           title="Upload Items" 
           :to="{ name: 'upload' }" 
@@ -39,15 +51,27 @@
           prepend-icon="mdi-chart-box" 
         />
       </v-list>
+
+      <template v-slot:append>
+        <v-divider class="mb-2" />
+        <v-list density="comfortable" nav>
+          <v-list-item 
+            title="Logout" 
+            prepend-icon="mdi-logout" 
+            @click="handleLogout" 
+          />
+        </v-list>
+      </template>
     </v-navigation-drawer>
 
-    <v-main>
+    <v-main class="pa-0">
       <v-container fluid>
         <router-view />
       </v-container>
     </v-main>
   </v-container>
 </template>
+
 <style scoped>
 .fill-height {
   min-height: 100vh;
